5cdc01a8aaa2dc7bf7b9dcc94166c8e818252b6c
Mariia <mariia.minieieva@gmail.com>
2025-10-06 20:49:22 -0500

    Part 4: A simple TUI (before rendering) done

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,5 +1,11 @@
 package edu.grinnell.csc207.texteditor;
 
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+
+import com.googlecode.lanterna.screen.Screen;
+
 /**
  * The driver for the TextEditor Application.
  */
@@ -8,14 +14,16 @@ public class TextEditor {
     /**
      * The main entry point for the TextEditor application.
      * @param args command-line arguments.
+     * @throws IOException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Compiled!");
         if (args.length != 1) {
             System.err.println("Usage: java TextEditor <filename>");
             System.exit(1);
         }
 
+        Screen screen = new DefaultTerminalFactory().createScreen();
         // TODO: fill me in with a text editor TUI!
         String path = args[0];
         System.out.format("Loading %s...\n", path);
